ef38d5be15aa610166cd2e7cabb1574aa0b0e3fe
Abdullah <abdullahwa786@gmail.com>
2023-03-15 17:26:59 +0430

    nothing

--- items/urls.py
+++ items/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.comps, name='companies')
+    path("", views.comps, name='companies'),
+    path("profile/<str:pk>", views.profile, name='profile'),
+    path("form/", views.newform, name='form'),
+
 ]
--- items/views.py
+++ items/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product, Companie
+from .models import Product, Companie, User
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -7,5 +8,17 @@ from .models import Product, Companie
 def comps(request):
     names = Companie.objects.only('name')
     context = {'names': names}
-
     return render(request, 'home.html', context)
+
+
+def profile(request, pk):
+    company = Companie.objects.get(id=pk)
+    products = company.product_set.all()
+    context = {'company': company, 'products': products}
+    return render(request, 'profile.html', context)
+
+
+def newform(request):
+    form = ProductForm
+
+    return render(request, 'new.html', {'form': form})
